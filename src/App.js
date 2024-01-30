490d23475dd1ae18b49f4f217b1ff24a5226fd9d
T350N <cahf2001@gmai.com>
2024-01-30 15:27:43 -0600

    project progress

---
+++ src/App.js
@@ -0,0 +1,28 @@
+
+import logo2 from './img/logo.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo2} className="App-logo" alt="logo" />
+        <p>
+          Hola <code>Bienvenidos a </code> Boxing ClubS 
+        </p>
+        {/*
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
